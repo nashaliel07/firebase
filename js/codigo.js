54bbb3693dfb4110496aced801e6bf5f7f3ff187
nasghaliel07 <62684201@certus.edu.pe>
2022-12-02 08:26:01 -0500

    first commit

---
+++ js/codigo.js
@@ -0,0 +1,5 @@
+document.getElementById(ingresar).addEventListener("click", function(){
+  let email = document.getElementById("email").value;
+  let pass = document.getElementById("pass").value;
+  
+})
